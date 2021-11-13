df1d936b028373f31e948b903fdd057213e8c019
Sergey <foxgroove1@mail.ru>
2021-11-13 12:24:56 +0300

    Final Task v1

--- task_final/solution.js
+++ task_final/solution.js
@@ -52,24 +52,26 @@ function sendRequest(count, name, phone, address, goods, sum) {
     return (jsonData);
 }*/
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: "Иван +7(987)65-43-210", goods: {title: "Пицца", count: "2"}, order: {"address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-            "sum": 900}};
-
+    let data = {client: "Иван +7(987)65-43-210", 
+                goods: [{title: "Пицца", count: 2}], 
+                order: {address: "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53", sum: 900}
+               };
+        
     data.client = name + ' ' + phone;
-    data.order.address = address;
+    data.order.address = `ул. ${address.street}, дом ${address.home}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     data.order.sum = sum;
-    data.goods.title = goods.title;
-    data.goods.count = goods.count;
-/*
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.goods[0].title = goods;
+    data.goods[0].count = goods.length;
 
-    data.client = 'Иван';*/
     let countOfGoods = goods.length;
+    let jsonData = JSON.stringify(data); //вывод строки - преобразование в строку массива данных
+
     for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+         goods.push(goods[i].title);
+      
     }
-    let jsonData = JSON.stringify(data);
+
+   // let jsonData = JSON.stringify(data); //вывод строки - преобразование в строку массива данных
 
     return jsonData;
 }
