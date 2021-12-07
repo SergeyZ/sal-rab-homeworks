1954f7e0c5535affe8bf040079c23af853d17d41
Sergey <foxgroove1@mail.ru>
2021-12-07 17:07:59 +0300

    Final Task v1.8

--- task_final/solution.js
+++ task_final/solution.js
@@ -36,21 +36,22 @@
 function sendRequest(name, phone, address, goods, sum) {
   let data = {goods:[],order:{}};
 
-  let countOfGoods = goods.length;    
+  let countOfGoods = goods.length;  
+ 
 
    data.client = name + ' ' + phone;
    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
    data.order.sum = sum; 
 
-   for (let i = 0; i <= countOfGoods; i += 1) {
-         data.goods.push(goods[i].title);
+   for (let i = 0; i < countOfGoods; i += 1) {
+         let tmp = {title: goods[i].title, count: goods[i].count}
+         data.goods.push(tmp);
    }
 
-   let jsonData = JSON.stringify(data);
+   let jsonData = JSON.stringify({data});
 
    return jsonData;
 }
-
 
 
 
